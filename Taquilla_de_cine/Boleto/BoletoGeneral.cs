b558ca399e12f9120d17c57ff9abd0f87b543130
ChristianJaredSantiagoHernandez <Cristianjared574@gmail.com>
2026-06-24 23:49:08 -0600

    # Agrega subclase BoletoGeneral heredada de boletos Calcula precio final sin descuento ($80.00)

---
+++ Taquilla_de_cine/Boleto/BoletoGeneral.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taquilla_de_cine.Boleto
+{
+    /// <summary>
+    /// Integrante D: Boleto tarifa general sin descuento
+    /// </summary>
+    public class BoletoGeneral : boletos
+    {
+        // Constructor: solo pasa el precio a la clase padre, SIN modificarlo
+        public BoletoGeneral(float precio_boleto) : base(precio_boleto)
+        {
+            // NO agregues nada aquí, así se mantiene el precio correcto
+        }
+
+        // Implementación del método: devuelve el precio completo
+        public override float calcularPrecioFinal()
+        {
+            return precioBoleto;
+        }
+    }
+}
